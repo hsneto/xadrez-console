96f24e7afa19b9e282672b4624cb362b3540d866
Humberto da Silva Neto <humberto.s.neto@hotmail.com>
2021-04-20 11:12:19 -0300

    Começando a colocar peças no tabuleiro

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -6,8 +6,8 @@
         public int qteMovimentos { get; protected set; }
         public Tabuleiro tab { get; set; }
 
-        public Peca(Posicao posicao, Tabuleiro tab, Cor cor) {
-            this.posicao = posicao;
+        public Peca(Tabuleiro tab, Cor cor) {
+            this.posicao = null;
             this.tab = tab;
             this.cor = cor;
             this.qteMovimentos = 0;
